a123ffbef9098d05ad41525f19ef66537303172a
Leonardo <37600692+uKaigo@users.noreply.github.com>
2022-04-10 23:19:21 -0300

    Fix bug with relative paths

--- zsh-plugins.plugin.zsh
+++ zsh-plugins.plugin.zsh
@@ -1,3 +1,5 @@
-source plugins/key-bindings.zsh
-source plugins/termsupport.zsh
-source plugins/vcs_info.zsh
+swd=$(dirname -- $(readlink -fn -- "$0"))
+
+source $swd/plugins/key-bindings.zsh
+source $swd/plugins/termsupport.zsh
+source $swd/plugins/vcs_info.zsh
