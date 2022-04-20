9c2fef06c72ef30b5b9be2996dfc970679eed8d3
Leonardo <37600692+uKaigo@users.noreply.github.com>
2022-04-20 00:21:48 -0300

    feat: add omz plugin support

--- zsh-plugins.plugin.zsh
+++ zsh-plugins.plugin.zsh
@@ -3,3 +3,7 @@ swd=$(dirname -- $(readlink -fn -- "$0"))
 source $swd/plugins/key-bindings.zsh
 source $swd/plugins/termsupport.zsh
 source $swd/plugins/vcs_info.zsh
+if [ ! -z $ZSH_PLUGINS_THEME ]; then
+  source $swd/plugins/git.zsh
+  source $swd/plugins/theme-and-appearence.zsh
+]
